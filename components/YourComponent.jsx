9325acf570e9c74c785da04b262e8cef628b44c3
JasubiPL <gameportal7@hotmail.com>
2023-02-16 12:15:42 -0600

    Create image component

---
+++ components/YourComponent.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+ const YourComponent = () =>{
+  return(
+    <Image
+    src='/image/profile.png'
+    height={144}
+    width={144}
+    />
+  )
+}
+
+export default YourComponent;
--- pages/post/first-post.jsx
+++ pages/post/first-post.jsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import YourComponent from "../../components/YourComponent";
+
 export default function FirstPost(){
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <YourComponent />
+      <h2><Link href='/'>Back to Home</Link></h2>
+    </>
+  )
 }
